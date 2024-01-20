ec7056c3441d1b11e9b4cfaf2a9bfd3fe1bf5175
Adam <80597881+Adam0845@users.noreply.github.com>
2024-01-21 00:50:40 +0100

    display, multiple add files
    added displaying of files properly, uploading multiple files option

--- FIles Manager Project v2/server.js
+++ FIles Manager Project v2/server.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express()
 const PORT = 3000;
-const fs = require("fs")
+const fs = require("fs");
+const formidable = require('formidable');
 const hbs = require('express-handlebars');
 const path = require("path")
 app.use(express.static('static'))
@@ -87,7 +88,23 @@ app.get("/filemanager", function (req, res) {
     })
     res.render('filemanager2.hbs', { filestab, dirs });
 })
+app.post('/upload', function (req, res) {
+    let form = formidable({});
+    form.keepExtensions = true
+    form.multiples = true
+    form.uploadDir = __dirname + '/upload';
+    form.keepFilenames = true;
+    form.parse(req, function (err, fields, files) {
+        Object.keys(files).forEach(function (name) {
+            const file = files[name];
+            const oldPath = file.path;
+            const newPath = path.join(form.uploadDir, file.name);
 
+            fs.renameSync(oldPath, newPath);
+        });
+    });
+    res.redirect("/filemanager")
+});
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
